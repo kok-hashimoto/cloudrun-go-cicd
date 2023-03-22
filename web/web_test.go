301c739a16e8026535a09a3a3515e2ead6fc445e
kok-hashimoto <kok-hashimoto@iret.co.jp>
2023-03-22 11:04:25 +0900

    add go test

---
+++ web/web_test.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestHello(t *testing.T) {
+	e := echo.New()
+	req := httptest.NewRequest(http.MethodGet, "/", nil)
+	rec := httptest.NewRecorder()
+	c := e.NewContext(req, rec)
+	assert.NoError(t, hello(c))
+	assert.Equal(t, http.StatusOK, rec.Code)
+	assert.Equal(t, "Hi, World!", rec.Body.String())
+}
